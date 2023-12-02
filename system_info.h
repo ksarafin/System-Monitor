0dcd3b3478d9ed546c442501cf77b3b40fecbe94
syekollu052 <90074986+syekollu052@users.noreply.github.com>
2023-12-01 22:17:42 -0800

    Create system_info.h

---
+++ system_info.h
@@ -0,0 +1,8 @@
+#ifndef SYSTEM_INFO_H
+#define SYSTEM_INFO_H
+
+unsigned long get_available_disk_space();
+void get_system_info(char* info_buffer, int buffer_size);
+void get_cpu_info(char* info_buffer, int buffer_size);
+
+#endif // SYSTEM_INFO_H
