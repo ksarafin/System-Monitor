ede45942a029ef5acee0e80b2472b9edb6c6665e
syekollu052 <90074986+syekollu052@users.noreply.github.com>
2023-12-03 16:47:43 -0800

    moved file system table to seperate file

--- system_monitor.c
+++ system_monitor.c
@@ -6,20 +6,8 @@
 #include <stdbool.h>
 #include <sys/vfs.h>
 
+#include "file_system_table.h"
 #include "process_table.h"
-
-// Define the number of columns in the table
-#define FILE_SYSTEM_NUM_COLUMNS 7
-
-typedef struct {
-  gchar *device;
-  gchar *directory;
-  gchar *type;
-  guint64 total_space;
-  guint64 free_space;
-  guint64 available_space;
-  guint64 used_space;
-} FileSystemData;
 
 const gchar *file_system_column_names[FILE_SYSTEM_NUM_COLUMNS] = {
   "Device",
@@ -30,102 +18,6 @@ const gchar *file_system_column_names[FILE_SYSTEM_NUM_COLUMNS] = {
   "Available",
   "Used"
 };
-
-/*void populate_file_system_table(GtkListStore *list_store) {
-  // Replace this with your actual file system data retrieval logic
-  // For now, let's use some dummy data
-  for (int i = 0; i < 5; ++i) {
-    FileSystemData data = {
-      .device = g_strdup_printf("/dev/sd%d", i),
-      .directory = g_strdup_printf("/mnt/fs%d", i),
-      .type = g_strdup("ext4"),
-      .free_space = 1024 * i,
-      .available_space = 512 * i,
-      .used_space = 512 * i
-    };
-
-    GtkTreeIter iter;
-    gtk_list_store_append(list_store, &iter);
-    gtk_list_store_set(list_store, &iter,
-        0, data.device,
-        1, data.directory,
-        2, data.type,
-        3, data.free_space,
-        4, data.available_space,
-        5, data.used_space,
-        -1);
-
-    g_free(data.device);
-    g_free(data.directory);
-    g_free(data.type);
-  }
-}*/
-
-
-void populate_file_system_table(GtkListStore *list_store) {
-    FILE *file = fopen("/proc/self/mountinfo", "r");
-    if (file == NULL) {
-        g_warning("Failed to open /proc/self/mountinfo");
-        return;
-    }
-
-    char line[1024];
-    while (fgets(line, sizeof(line), file) != NULL) {
-        // Tokenize the line using whitespace as a delimiter
-        char *token;
-        char *tokens[12];  // Increase the array size to accommodate all expected tokens
-        int tokenCount = 0;
-
-        token = strtok(line, " ");
-        while (token != NULL && tokenCount < 12) {
-            tokens[tokenCount++] = token;
-            token = strtok(NULL, " ");
-        }
-
-        if (tokenCount >= 11) {
-            gchar *device_name = g_strdup(tokens[4]);
-
-            struct statfs sb;
-
-            statfs(tokens[4],&sb);
-
-            if (device_name != NULL) {
-                FileSystemData data = {
-                    .device = device_name,
-                    .directory = g_strdup(tokens[9]),
-                    .type = g_strdup(tokens[8]),
-                    .total_space = sb.f_blocks,
-                    .free_space = sb.f_bfree,
-                    .available_space = sb.f_bavail,
-                    .used_space = sb.f_blocks - sb.f_bfree
-                };
-
-                // Print debug information
-                //g_print("Device: %s, Directory: %s, Type: %s, Free: %lu, Available: %lu, Used: %lu\n",
-                  //      data.device, data.directory, data.type, data.free_space, data.available_space, data.used_space);
-
-                GtkTreeIter iter;
-                gtk_list_store_append(list_store, &iter);
-                gtk_list_store_set(list_store, &iter,
-                                    0, data.device,
-                                    1, data.directory,
-                                    2, data.type,
-                                    3, data.total_space,
-                                    4, data.free_space,
-                                    5, data.available_space,
-                                    6, data.used_space,
-                                    -1);
-
-                // Free the memory allocated for the strings in data
-                g_free(data.device);
-                g_free(data.directory);
-                g_free(data.type);
-            }
-        }
-    }
-
-    fclose(file);
-}
 
 void on_process_right_click(GtkTreeView *treeview, GdkEventButton *event, gpointer user_data) {
     if (event->type == GDK_BUTTON_PRESS && event->button == GDK_BUTTON_SECONDARY) {
