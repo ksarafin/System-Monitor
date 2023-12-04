657d931713b0ac09846f80beeaf0ce7ebde560fa
syekollu052 <90074986+syekollu052@users.noreply.github.com>
2023-12-04 14:06:42 -0800

    Create resource_graph.h

---
+++ resource_graph.h
@@ -0,0 +1,10 @@
+#ifndef RESOURCE_GRAPH_H
+#define RESOURCE_GRAPH_H
+
+#include <gtk/gtk.h>
+
+#define UPDATE_INTERVAL 1000 // Update interval in milliseconds
+
+GtkWidget *create_usage_page();
+
+#endif
