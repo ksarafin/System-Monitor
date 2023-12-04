bab7ca71c062b64250731e536a55985c7827435e
syekollu052 <syekollu@purdue.edu>
2023-12-04 14:05:09 -0800

    update process_table.h

--- process_table.h
+++ process_table.h
@@ -7,8 +7,20 @@
 
 #define NUM_COLUMNS 5
 
+// Enum to indicate process filter
+typedef enum {
+    ALL_PROCESSES,
+    USER_PROCESSES,
+    ACTIVE_PROCESSES
+} ProcessListType;
+extern ProcessListType process_type;
+
 extern const gchar *column_names[NUM_COLUMNS];
 
 void populate_process_table(GtkListStore *list_store);
+GtkWidget *createProcessesPage();
+void showProcessDetailsDialog(int pid);
+void on_process_right_click(GtkTreeView *treeview, GdkEventButton *event, gpointer user_data);
+void on_process_double_click(GtkTreeView *treeview, GtkTreePath *path, GtkTreeViewColumn *column, gpointer user_data);
 
 #endif // PROCESS_TABLE_H
