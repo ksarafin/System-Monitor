9e79f4bf7c55261228bd97e1c06372f8874bac30
syekollu052 <90074986+syekollu052@users.noreply.github.com>
2023-12-03 16:48:09 -0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-gcc $(pkg-config --cflags gtk+-3.0) system_monitor.c system_info.c process_table.c $(pkg-config --libs gtk+-3.0)
+gcc $(pkg-config --cflags gtk+-3.0) system_monitor.c system_info.c process_table.c file_system_table.c $(pkg-config --libs gtk+-3.0)
