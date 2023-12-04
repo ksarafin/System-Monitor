40d7c5d39eb4bbbb954a071d9c941617736f7731
syekollu052 <90074986+syekollu052@users.noreply.github.com>
2023-12-03 16:49:26 -0800

    made window size bigger

--- system_monitor.c
+++ system_monitor.c
@@ -82,7 +82,7 @@ int main(int argc, char *argv[]) {
   // Create the main window
   GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW(window), "System Monitor");
-  gtk_window_set_default_size(GTK_WINDOW(window), 700, 900);
+  gtk_window_set_default_size(GTK_WINDOW(window), 900, 1100);
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
   // Create a vertical box to organize widgets
