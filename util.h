3a929e965502442ab42183d915b355d42f9622d5
syekollu052 <90074986+syekollu052@users.noreply.github.com>
2023-12-04 14:00:01 -0800

    Create util.h

---
+++ util.h
@@ -0,0 +1,12 @@
+#ifndef SYSTEM_UTIL_H
+#define SYSTEM_UTIL_H
+
+
+
+double get_cpu_usage();
+double get_memory_usage();
+double get_network_usage();
+double get_process_cpu_usage(int pid);
+
+
+#endif
