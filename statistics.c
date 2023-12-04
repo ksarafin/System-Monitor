10723cbab102facb8a05ba4bbe34b8efc7e5a83d
emkwan <90022771+emkwan@users.noreply.github.com>
2023-12-04 00:19:45 -0500

    Update statistics.c

--- statistics.c
+++ statistics.c
@@ -3,9 +3,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <gtk/gtk.h>
-
 #include "statistics.h"
-
 
 int g_num_samples = 0; 
 int g_delay_seconds = 0;
@@ -18,6 +16,9 @@ GtkWidget *memory_label;
 GtkWidget *memory_progress;
 GtkWidget *swap_label;
 GtkWidget *swap_progress;
+GtkWidget *cpu_graph;
+GtkWidget *memory_graph;
+GtkWidget *swap_graph;
 
 
 /* Reads from /proc/meminfo, checks if certain fields are there, and calculates memory + swap usages */
@@ -56,6 +57,64 @@ void calculate_memory_swap_usage(double *memory_usage, double *swap_usage) {
     *swap_usage = ((total_swap - free_swap) * 100.0) / total_swap;
 }
 
+gboolean draw_memory_graph(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
+    // Retrieve memory usage value
+    double memory_usage, swap_usage;
+    calculate_memory_swap_usage(&memory_usage, &swap_usage);
+
+    // Get the size of the drawing area
+    int width, height;
+    gtk_widget_get_size_request(widget, &width, &height);
+
+    // Calculate the position for the line
+    double x = width / 2.0;  // Center of the drawing area
+    double y = height * (1 - memory_usage / 100.0);  // Adjust the scale
+
+    // Set line color and width
+    cairo_set_source_rgb(cr, 1.0, 0.0, 0.0);  // Red color
+    cairo_set_line_width(cr, 2.0);
+
+    // Move to the starting point
+    cairo_move_to(cr, 0, height);
+
+    // Draw a line to the calculated position
+    cairo_line_to(cr, x, y);
+
+    // Stroke the path (draw the line)
+    cairo_stroke(cr);
+
+    return FALSE;
+}
+
+gboolean draw_swap_graph(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
+    // Retrieve swap usage value
+    double memory_usage, swap_usage;
+    calculate_memory_swap_usage(&memory_usage, &swap_usage);
+
+    // Get the size of the drawing area
+    int width, height;
+    gtk_widget_get_size_request(widget, &width, &height);
+
+    // Calculate the position for the line
+    double x = width / 2.0;  // Center of the drawing area
+    double y = height * (1 - swap_usage / 100.0);  // Adjust the scale
+
+    // Set line color and width
+    cairo_set_source_rgb(cr, 0.0, 1.0, 0.0);  // Green color
+    cairo_set_line_width(cr, 2.0);
+
+    // Move to the starting point
+    cairo_move_to(cr, 0, height);
+
+    // Draw a line to the calculated position
+    cairo_line_to(cr, x, y);
+
+    // Stroke the path (draw the line)
+    cairo_stroke(cr);
+
+    return FALSE;
+}
+
 /* calculate network usage by reading from /proc/net/dev and taking the necessary info (parsing manually) */
 void calculate_network_usage() {
     // Open /proc/net/dev for reading
@@ -88,6 +147,34 @@ void calculate_network_usage() {
 
     // Print or store the network usage
     printf("Received Bytes: %ld, Transmitted Bytes: %ld\n", received_bytes, transmitted_bytes);
+}
+
+gboolean draw_network_graph(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
+    // Placeholder for network usage value (modify as per your network calculation)
+    double network_usage = 0.0;
+
+    // Get the size of the drawing area
+    int width, height;
+    gtk_widget_get_size_request(widget, &width, &height);
+
+    // Calculate the position for the line
+    double x = width / 2.0;  // Center of the drawing area
+    double y = height * (1 - network_usage / 100.0);  // Adjust the scale
+
+    // Set line color and width
+    cairo_set_source_rgb(cr, 1.0, 1.0, 0.0);  // Yellow color
+    cairo_set_line_width(cr, 2.0);
+
+    // Move to the starting point
+    cairo_move_to(cr, 0, height);
+
+    // Draw a line to the calculated position
+    cairo_line_to(cr, x, y);
+
+    // Stroke the path (draw the line)
+    cairo_stroke(cr);
+
+    return FALSE;
 }
 
 /* 
@@ -142,6 +229,34 @@ double calculate_cpu_usage() {
     return 100 - idle_percentage;
 }
 
+gboolean draw_cpu_graph(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
+    // Retrieve CPU usage value
+    double cpu_usage = calculate_cpu_usage();
+
+    // Get the size of the drawing area
+    int width, height;
+    gtk_widget_get_size_request(widget, &width, &height);
+
+    // Calculate the position for the line
+    double x = width / 2.0;  // Center of the drawing area
+    double y = height * (1 - cpu_usage / 100.0);  // Adjust the scale
+
+    // Set line color and width
+    cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);  // Blue color
+    cairo_set_line_width(cr, 2.0);
+
+    // Move to the starting point
+    cairo_move_to(cr, 0, height);
+
+    // Draw a line to the calculated position
+    cairo_line_to(cr, x, y);
+
+    // Stroke the path (draw the line)
+    cairo_stroke(cr);
+
+    return FALSE;
+}
+
 /* incorporating gtk where we take our calculations and update the GUI with them */
 void update_stats() {
     double cpu_usage = 0.0;
@@ -172,6 +287,20 @@ void update_stats() {
     gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(swap_progress), swap_usage / 100.0);
 }
 
+void create_graph(GtkWidget *grid, GtkWidget **graph, const gchar *label_text, GCallback draw_func) {
+    // Label for the graph
+    GtkWidget *graph_label = gtk_label_new(label_text);
+    gtk_grid_attach(GTK_GRID(grid), graph_label, 0, 3, 1, 1);
+
+    // Graph drawing area
+    *graph = gtk_drawing_area_new();
+    gtk_widget_set_size_request(*graph, 200, 100);  // Set the size as needed
+    gtk_grid_attach(GTK_GRID(grid), *graph, 1, 3, 1, 1);
+
+    // Connect the draw signal to the draw function
+    g_signal_connect(*graph, "draw", draw_func, NULL);
+}
+
 /* main function -> format the gtk GUI, periodically updating it with refreshed stats */
 int main(int argc, char *argv[]) {
     // check if the correct number of command-line arguments is provided
@@ -198,26 +327,40 @@ int main(int argc, char *argv[]) {
     gtk_container_add(GTK_CONTAINER(window), grid);
 
     // labels + progress bars for:
+
+    // 1. CPU Usage
     cpu_label = gtk_label_new("CPU Usage:");
     gtk_grid_attach(GTK_GRID(grid), cpu_label, 0, 0, 1, 1);
 
-    // 1. CPU Usage
     cpu_progress = gtk_progress_bar_new();
     gtk_grid_attach(GTK_GRID(grid), cpu_progress, 1, 0, 1, 1);
 
+    // 2. Memory Usage
     memory_label = gtk_label_new("Memory Usage:");
     gtk_grid_attach(GTK_GRID(grid), memory_label, 0, 1, 1, 1);
 
-    // 2. Memory Usage
     memory_progress = gtk_progress_bar_new();
     gtk_grid_attach(GTK_GRID(grid), memory_progress, 1, 1, 1, 1);
 
+    // 3. Swap Usage
     swap_label = gtk_label_new("Swap Usage:");
     gtk_grid_attach(GTK_GRID(grid), swap_label, 0, 2, 1, 1);
 
-    // 3. Swap Usage
     swap_progress = gtk_progress_bar_new();
     gtk_grid_attach(GTK_GRID(grid), swap_progress, 1, 2, 1, 1);
+
+    // 4. Network Usage
+    GtkWidget *network_label = gtk_label_new("Network Usage:");
+    gtk_grid_attach(GTK_GRID(grid), network_label, 0, 4, 1, 1);
+
+    GtkWidget *network_progress = gtk_progress_bar_new();
+    gtk_grid_attach(GTK_GRID(grid), network_progress, 1, 4, 1, 1);
+
+    // Graphs
+    create_graph(grid, &cpu_graph, "CPU Graph:", G_CALLBACK(draw_cpu_graph));
+    create_graph(grid, &memory_graph, "Memory Graph:", G_CALLBACK(draw_memory_graph));
+    create_graph(grid, &swap_graph, "Swap Graph:", G_CALLBACK(draw_swap_graph));
+    create_graph(grid, &network_graph, "Network Graph:", G_CALLBACK(draw_network_graph));
 
     // timer allowing us to update stats at certain time intervals
     guint timer_id = g_timeout_add_seconds(g_delay_seconds, (GSourceFunc)upstate_stats, window);
